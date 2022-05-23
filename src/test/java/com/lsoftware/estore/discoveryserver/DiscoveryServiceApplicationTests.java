4b93e03b42fcf212a28cd46f5cfe10b00a49bf71
Luis Espinosa <lluisllanoss@gmail.com>
2022-05-23 02:53:12 -0500

    Happy path achived

---
+++ src/test/java/com/lsoftware/estore/discoveryserver/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsoftware.estore.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
